1a27b85031a63f7ab691c649c696508431236365
Luca Mattivi <luca@smartdomotik.com>
2016-08-12 12:32:45 +0200

    Add Basic Auth Logic

--- start.sh
+++ start.sh
@@ -5,7 +5,15 @@ chmod +x $PROJECT_PATH/generate_env.sh
 /bin/bash $PROJECT_PATH/generate_env.sh >> $PROJECT_PATH/.env
 
 
-envsubst < .htpasswd
+echo $HTPASSWD > $PROJECT_PATH/.htpasswd
+
+if [ "$BASIC_AUTH" == "1" ]
+    then
+        sed -i '/Satisfy/s/^/#/g' $PROJECT_PATH/public/.htaccess
+    else
+        sed -i '/Satisfy/s/#//g' $PROJECT_PATH/public/.htaccess
+fi
+
 
 
 exec /usr/sbin/apache2ctl -D FOREGROUND
